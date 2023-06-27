45d70fe2ba139ea6383372f38845c4a2b30e4c82
Geovanny Gil <geovannygr2018@gmail.com>
2023-06-26 21:10:16 -0600

    config for pg

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,14 +2,14 @@
 module.exports = ({ env }) => {
   return {
     connection: {
-      client: 'mysql',
+      client: 'postgres',
       connection: {
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: false
+        ssl: { rejectUnauthorized: false },
       },
       debug: false,
     },
