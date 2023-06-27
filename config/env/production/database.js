18ec51c0ac8a0d53f016f810fbd64fdb2ff11f9c
Geovanny Gil <geovannygr2018@gmail.com>
2023-06-26 19:04:51 -0600

    update db and server config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,19 @@
 
 module.exports = ({ env }) => {
   return {
-    connection: {
-      client: 'mysql',
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: { rejectUnauthorized: false },
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          settings: {
+            host: env('DATABASE_HOST', 'localhost'),
+            port: env.int('DATABASE_PORT', 3306),
+            database: env('DATABASE_NAME', 'strapi'),
+            user: env('DATABASE_USERNAME', 'strapi'),
+            password: env('DATABASE_PASSWORD', 'strapi'),
+            ssl: { rejectUnauthorized: false },
+          }
+        }
       },
       debug: false
     }
-  }
 };
