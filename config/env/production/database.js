f3553cd827969528bd8d24f390a639934d2b92ff
Geovanny Gil <geovannygr2018@gmail.com>
2023-06-26 20:57:17 -0600

    test config database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: { rejectUnauthorized: false },
+        ssl: false
       },
       debug: false,
     },
