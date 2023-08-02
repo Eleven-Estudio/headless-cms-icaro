153cc2ddecba2bfbc2e8210297a50d86dee742fb
Geovanny Gil <geovannygr2018@gmail.com>
2023-08-02 01:28:53 -0600

    feat: add news previes to security policy

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,14 @@
-module.exports = ({ env }) => ([
-  'strapi::errors',
+module.exports = ({ env }) => [
+  "strapi::errors",
   // 'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
   {
     name: "strapi::security",
     config: {
@@ -22,11 +22,19 @@ module.exports = ({ env }) => ([
             "blob:",
             env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, ""),
           ],
-          // Add for youtubue embeds
+          // Add for youtubue, facebook, twitter, vimeo, miro, twitch
           "frame-src": [
             "'self'",
             "https://www.youtube.com",
             "https://www.youtube-nocookie.com",
+            "https://www.facebook.com",
+            "https://platform.twitter.com",
+            "https://syndication.twitter.com",
+            "https://www.instagram.com",
+            "https://player.vimeo.com",
+            "https://miro.com",
+            "https://www.twitch.tv",
+            "https://player.twitch.tv",
           ],
           "media-src": [
             "'self'",
@@ -39,4 +47,4 @@ module.exports = ({ env }) => ([
       },
     },
   },
-]);
+];
